9135a30972ee5ee04f59a9b985cf9fed7ccf3eac
Guilherme Silva <guidss3394@gmail.com>
2023-01-03 14:52:46 -0300

    block para numeros negativos nas peças

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,7 +45,9 @@ function manipulaDados(operacao, controle) {
     const peca = controle.querySelector("[data-contador]")
 
     if (operacao === "-")  {
-        peca.value = parseInt(peca.value) - 1
+        if (peca.value > 0) {
+            peca.value = parseInt(peca.value) - 1
+        }
     } else {
         peca.value = parseInt(peca.value) + 1
     }
