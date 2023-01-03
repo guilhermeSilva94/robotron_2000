30c7261ca1adef684e4c33e3473b139532795c70
Guilherme Silva <guidss3394@gmail.com>
2023-01-02 21:04:38 -0300

    correção de ortografia

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,7 +44,7 @@ controle.forEach( (elemento) => {
 function manipulaDados(operacao, controle) {
     const peca = controle.querySelector("[data-contador]")
 
-    if (operacao === "-") {
+    if (operacao === "-")  {
         peca.value = parseInt(peca.value) - 1
     } else {
         peca.value = parseInt(peca.value) + 1
